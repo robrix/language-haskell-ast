968d0a964faa3b5510c3846c3d66cfd98bd3ca4d
Rob Rix <rob.rix@github.com>
2016-08-27 17:26:33 -0400

    Add an IsAST instance over Module SrcSpanInfo.

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,8 @@ module Main where
 import Data.Monoid
 import GHC.Generics
 import Language.Haskell.Exts
+import Language.Haskell.Exts.SrcLoc
+import Language.Haskell.Exts.Syntax
 
 path = "/Users/rob/Developer/Projects/language-haskell-ast/app/Main.hs"
 
@@ -16,6 +18,10 @@ main = do
 
 data AST = AST { astLoc :: SrcSpanInfo, astName :: String, astChildren :: [AST] }
   deriving (Eq, Show)
+
+instance IsAST (Module SrcSpanInfo) where
+  toAST m@(Module l _ _ _ _) = AST l "program" []
+
 
 class IsAST t where
   toAST :: t -> AST
