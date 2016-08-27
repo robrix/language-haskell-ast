f039a665d60c0634cdef5194bafa38c9af68abc8
Rob Rix <rob.rix@github.com>
2016-08-27 17:08:55 -0400

    Set up to derive IsAST instances generically.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE DefaultSignatures, DeriveAnyClass, FlexibleContexts, FlexibleInstances, StandaloneDeriving #-}
 module Main where
 
 import Data.Monoid
+import GHC.Generics
 import Language.Haskell.Exts
 
 path = "/Users/rob/Developer/Projects/language-haskell-ast/app/Main.hs"
@@ -16,3 +18,8 @@ data AST = AST { loc :: SrcLoc, name :: String, children :: AST }
 
 class IsAST t where
   toAST :: t -> AST
+  default toAST :: (Generic t, IsAST' (Rep t)) => t -> AST
+  toAST = toASTGeneric
+
+class IsAST' t where
+  toAST' :: t a -> AST
