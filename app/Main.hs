941b4376fb96b82caa9296af83639a9f4e3ae4f8
Rob Rix <rob.rix@github.com>
2016-08-28 01:06:08 -0400

    Define IsAST over QOp.

--- app/Main.hs
+++ app/Main.hs
@@ -57,6 +57,7 @@ instance IsAST Overlap
 instance IsAST FunDep
 instance IsAST Activation
 instance IsAST Assoc
+instance IsAST QOp
 
 instance IsAST Annotation where
   toAST (Ann l name e) = Branch l "expression_annotation" [ toAST name{-, toAST e -} ]
