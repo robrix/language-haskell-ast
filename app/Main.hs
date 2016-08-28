64bcc7dc4191e7b47b347e586499850190b8f716
Rob Rix <rob.rix@github.com>
2016-08-28 01:01:15 -0400

    Define IsAST over Safety.

--- app/Main.hs
+++ app/Main.hs
@@ -52,6 +52,7 @@ instance IsAST InjectivityInfo
 instance IsAST ResultSig
 instance IsAST Op
 instance IsAST DataOrNew
+instance IsAST Safety
 
 instance IsAST Annotation where
   toAST (Ann l name e) = Branch l "expression_annotation" [ toAST name{-, toAST e -} ]
