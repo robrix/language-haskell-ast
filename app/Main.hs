d17ba57e54c2085874d2326228c14336009d9d82
Rob Rix <rob.rix@github.com>
2016-08-27 20:43:37 -0400

    Define the IsAST' instance over M1 C to produce leaves where appropriate.

--- app/Main.hs
+++ app/Main.hs
@@ -132,8 +132,9 @@ toASTGeneric = toAST' . from
 instance (IsLocated f, IsAST' f, Datatype c) => IsAST' (M1 D c f) where
   toAST' m = toAST' (unM1 m)
 
-instance (IsLocated f, IsAST'' f, Constructor c) => IsAST' (M1 C c f) where
-  toAST' m = Branch (location m) (conName m) (toAST'' (unM1 m))
+instance (IsLocated f, IsLeaf g, IsAST'' f, Constructor c) => IsAST' (M1 C c (f :*: g)) where
+  toAST' m = Leaf (location m) (conName m) (leaf (r (unM1 m)))
+    where r (_ :*: r) = r
 
 instance IsAST'' (M1 S c (K1 R v)) where
   toAST'' m = []
