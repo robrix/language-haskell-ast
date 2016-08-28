720a8b3435907aac5a03d76825e9a76801e583c0
Rob Rix <rob.rix@github.com>
2016-08-28 00:18:13 -0400

    Define IsAST over locatable constructors without bodies.

--- app/Main.hs
+++ app/Main.hs
@@ -104,6 +104,9 @@ instance Constructor c => IsAST (C1 c (S1 s (Rec0 SrcRange) :*: (S1 t (Rec0 Stri
   toAST m = Leaf (location m) (conName m) $ unK1 . unM1 . r . unM1 $ m
     where r (_ :*: r) = r
 
+instance Constructor c => IsAST (C1 c (S1 s (Rec0 SrcRange))) where
+  toAST m = Branch (location m) (conName m) []
+
 instance (IsLocated l, Constructor c, IsAST v) => IsAST (C1 c (l :*: (S1 t (Rec0 (v SrcRange))))) where
   toAST m = Branch (location m) (conName m) $ pure . toAST . unK1 . unM1 . r . unM1 $ m
     where r (_ :*: r) = r
