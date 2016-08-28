4abf8f56e6e65aaa8213f729583234f7ba52da59
Rob Rix <rob.rix@github.com>
2016-08-28 00:49:59 -0400

    Define IsAST' over Maybe […] fields.

--- app/Main.hs
+++ app/Main.hs
@@ -122,6 +122,9 @@ instance (IsAST v, Selector s) => IsAST' (S1 s (Rec0 [v SrcRange])) where
 instance (IsAST v, Selector s) => IsAST' (S1 s (Rec0 (Maybe (v SrcRange)))) where
   toAST' = fmap toAST . maybeToList . unK1 . unM1
 
+instance (IsAST v, Selector s) => IsAST' (S1 s (Rec0 (Maybe [v SrcRange]))) where
+  toAST' = fmap toAST . join . maybeToList . unK1 . unM1
+
 instance Selector s => IsAST' (S1 s (Rec0 SrcRange))
 instance Selector s => IsAST' (S1 s (Rec0 String))
 instance Selector s => IsAST' (S1 s (Rec0 Bool))
